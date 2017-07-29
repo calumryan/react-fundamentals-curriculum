5cf69e442d9b35841c7969ebdee8b18ad3779d6d
Calum Ryan <hello@calumryan.com>
2017-07-29 14:07:56 +0100

    HW example

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Hello Word 2017</h1>
+      </div>
+    )
+  }
+}
+
+module.exports = App;
